1c55f7295ff7a896a1f0150bab8b86e2807b8756
Vileblood-sudo <spt42@outlook.com>
2021-04-11 21:38:33 -0300

    check for open ssh service

--- data/qscp.sh
+++ data/qscp.sh
@@ -9,25 +9,44 @@ cat <<EOF
   \___\_\\__,_|_|\___|_|\_\_____/ \_____|_|     
             2021 - Steven Thompson
 EOF
-DELAY=5
+DELAY=3
+echo
+echo "Ensure SSH is open on connecting server"
 echo
 read -p "Please input the IP Address:  " ipInput
 read -p "Please input the username:  " usernameInput
 read -p "Please input the port:  " portInput
 
+echo
+echo "Checking if SSH is open..."
+echo
+exec 3>/dev/tcp/${ipInput}/${portInput} > /dev/null 2>&1
+if [ $? -eq 0 ]; then
+    echo
+    echo "SSH is Open!"
+    sleep $DELAY
+else
+    echo
+    echo "SSH is Closed!"
+    sleep $DELAY
+    clear
+    ./qscp.sh
+fi
+
 while [[ "$REPLY" != 0 ]]; do
 	clear
-	cat <<-_EOF_
+	cat <<EOF
 	Please select from the following options:
 
     1.) Copy a file to a remote system
     2.) Copy a file from a remote system
+    3.) Change server data
 	
-	_EOF_
+EOF
   
-	read -p "Enter your selection [1-2]> "
+	read -p "Enter your selection [1-3]> "
   
-	if [[ "$REPLY" =~ ^[1-2]$ ]]; then
+	if [[ "$REPLY" =~ ^[1-3]$ ]]; then
         if [[ "$REPLY" =~ ^[1]$ ]]; then
             echo "You have selected to copy a file to a remote system"
             echo
@@ -48,5 +67,11 @@ while [[ "$REPLY" != 0 ]]; do
             echo "Copy Successful"
             sleep $DELAY
         fi
+        if [[ "$REPLY" =~ ^[3]$ ]]; then
+            ./qscp.sh
+        fi
+        else
+            echo "Input Error. Please try again.."
+            sleep $DELAY
     fi
 done
